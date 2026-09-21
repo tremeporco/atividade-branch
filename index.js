6524af6d76fe8c1700036deff5f8b8d50640cfb0
Eduardo Silva Passarelo <eduardo.passarelo@estudante.ifms.edu.br>
2026-09-21 17:58:45 +0000

    dividir 1

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const subtrair = (a, b) => a - b;
 
 // 3. Multiplicação
 const multiplicar = (a, b) => a * b;
+
+// 4. Divisão 
+const dividir = (a, b) => {
+    if (b === 0) return "Erro: Divisão por zero";
+    return a / b;
